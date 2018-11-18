ff2adb7b3c9465809060690599c0107b142905da
s greenbaum <gbs4ever2@gmail.com>
2018-11-18 23:50:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function  makeInt(n){ return parseInt(n,10);
 
   }
 
-function preserveDecimal(n) {return parseFloat(n);}
+function preserveDecimal(n) {return parseFloat(2.222);}
 
-function preserveDecimal(n) {return parseFloat('thth')}
+
