69dfd2a5e0c01dbcd38d7e09b16609c1221de20a
s greenbaum <gbs4ever2@gmail.com>
2018-11-18 23:07:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function add(a,b){ return a+b}
+function subtract(a,b){ return a-b}
+function multiply(a,b){ return a*b}
+function divide(a, b){ return a/b}
+
+
+function inc(a){ return a+1}
+function dec(a){ return a-1}
+
+
+function  makeInt(n){ return parseInt('5');}
+function  makeInt(n){ return parseInt('0',10);}
+
+function  makeInt(n){ return parseInt('sldkjflksjf, 15');}
+function preserveDecimal(n) {return parseFloat(2.222);}
+
+function preserveDecimal(n) {return parseFloat('thth')}
