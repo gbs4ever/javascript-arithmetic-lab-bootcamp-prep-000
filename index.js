0689094de4107038add59863cfc5ce08fe2d4ca6
s greenbaum <gbs4ever2@gmail.com>
2018-11-18 23:22:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function inc(a){ return a+1}
 function dec(a){ return a-1}
 
 
-function  makeInt(a){ console.console.log(parseInt('5',10))
+function  makeInt(a){ console.log(parseInt('5',10))
   return parseInt('sldkjflksjf, 15');}
 function preserveDecimal(n) {return parseFloat(2.222);}
 
